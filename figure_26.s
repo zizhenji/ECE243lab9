cb01d989bc4dd3a60c0fb27cb7c4edc2466dcad9
Zizhen Ji <zizhen.ji@mail.utoronto.ca>
2022-04-03 15:33:45 -0400

    initial commit

---
+++ figure_26.s
@@ -0,0 +1,9 @@
+START:  mv    sp, =0x1000   // sp = 0x1000 = 4096
+        mv    r4, =0x0F0F
+        push  r4
+        bl    SUBR
+        pop   r4
+END:    b     END
+
+SUBR:   sub   r4, r4
+        mv    pc, lr
